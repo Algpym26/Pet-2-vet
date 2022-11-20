e7af2c4c177f46041f565c15a81287177efc6d9c
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-19 19:26:34 -0500

    code to model files

--- models/Cat.js
+++ models/Cat.js
@@ -1,3 +1,54 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+// id
+// name
+// Declawed
+// age
+//weight
+
+class Cat extends Model {}
+
+Cat.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey:true,
+            autoIncrement: true,
+        },
+
+        name: {
+            type: DataTypes.VARCHAR,
+            allowNull: false,
+        },
+
+        Declawed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+
+        },
+
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+
+        },
+
+        weight: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        }
+
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Cat',
+      }
+)
+
+module.exports = Cat;
