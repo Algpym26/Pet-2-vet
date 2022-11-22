d58210cc54aef65263107fca502ca029fbae7bf8
allenmurfee <allenmurfee123@gmail.com>
2022-11-21 19:13:42 -0500

    working on patient page routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const loginRoutes = require("./loginPage");
+const patientRoutes = require("./patientsPage");
 
 router.use("/api", apiRoutes);
+router.use("/login", loginRoutes);
+router.use("/patients", patientRoutes);
 
 module.exports = router;
