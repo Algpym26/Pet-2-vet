dc26bf0b33a7ac3ed4dc45566dd4bd39531d3f27
allenmurfee <allenmurfee123@gmail.com>
2022-11-21 19:58:50 -0500

    worked more on cat and dog route organization

--- controllers/patients/catRoutes.js
+++ controllers/patients/catRoutes.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+const { Cat, Dog, Owner } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
   try {
-  } catch (err) {}
+    const newCat = await Cat.create({
+    
+    })
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/patients/dogRoutes.js
+++ controllers/patients/dogRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const { Cat, Dog, Owner } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
   try {
-  } catch (err) {}
+    const newDog = await Dog.create({});
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 module.exports = router;
