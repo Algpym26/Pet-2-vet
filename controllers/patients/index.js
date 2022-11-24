a97de524e323ba90bbb09991e9f518c891f79e3b
Katie <thompsonkatherine704@gmail.com>
2022-11-23 20:07:05 -0500

    updated patientspage handlebars, patient routes, and index routes

--- controllers/patients/index.js
+++ controllers/patients/index.js
@@ -6,24 +6,6 @@ const dogRoutes = require("./dogRoutes");
 // router.use("/", patientRoutes);
 router.use("/cat", catRoutes);
 router.use("/dog", dogRoutes);
-
-router.get("/", async (req, res) => {
-    console.log("firing")
-    try {
-    //   const petData = await Owner.findAll({
-    //     include: [{ model: Dog }, { model: Cat }],
-    //   });
-  
-    //   const pets = projectData.map((pet) => pet.get({ plain: true }));
-  
-      res.render("patientsPage");
-      // {
-      //   pets,
-      //   logged_in: req.session.logged_in,
-      // });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.use("/", patientRoutes);
 
 module.exports = router;
