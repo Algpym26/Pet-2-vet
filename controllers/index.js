88ce6fd1ada5cbd180d83b30670c5d107d9a26d2
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-28 18:40:12 -0500

    completed services page

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const patientRoutes = require("./patients");
 const userRoutes = require("./userRoutes");
+const servicesRoutes = require("./servicesRoutes");
 
 router.use("/patients", patientRoutes);
+router.use("/services", servicesRoutes)
 router.use("/", userRoutes);
 
 module.exports = router;
