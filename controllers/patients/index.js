6632c64774bb4ffe9a070178ce558a0a854a2e38
allenmurfee <allenmurfee123@gmail.com>
2022-11-29 13:15:39 -0500

    still having bug

--- controllers/patients/index.js
+++ controllers/patients/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const patientRoutes = require("./patientRoutes");
-const petRoutes = require("./petRoutes")
+const petRoutes = require("./petRoutes/index")
 
 router.use("/", patientRoutes);
 router.use("/add", petRoutes);
