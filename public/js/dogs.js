d9f338b3b51db8e7b44f5e4d76c2d6f979d6515d
allenmurfee <allenmurfee123@gmail.com>
2022-11-29 21:33:14 -0500

    cat route kind of working

--- public/js/dogs.js
+++ public/js/dogs.js
@@ -18,27 +18,26 @@ const addDogHandler = async (event) => {
   }
 };
 
-const addCatHandler = async (event) => {
-  event.preventDefault();
+// const addCatHandler = async (event) => {
+//   console.log("cat button working")
+//   event.preventDefault();
 
-  const name = document.querySelector("#name").value.trim();
-  const declawed = document.querySelector("#declawed").value.trim();
-  const age = document.querySelector("#age").value.trim();
-  const weight = document.querySelector("#weight").value.trim();
+//   const name = document.querySelector("#name").value.trim();
+//   const declawed = document.querySelector("#declawed").value.trim();
+//   const age = document.querySelector("#age").value.trim();
+//   const weight = document.querySelector("#weight").value.trim();
 
-  const response = await fetch("/patients/add-cat", {
-    method: "POST",
-    body: JSON.stringify({ name, declawed, age, weight }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-    document.location.replace("/patients");
-  } else {
-    alert(response.statusText);
-  }
-};
-
-
+//   const response = await fetch("/patients/add-cat", {
+//     method: "POST",
+//     body: JSON.stringify({ name, declawed, age, weight }),
+//     headers: { "Content-Type": "application/json" },
+//   });
+//   if (response.ok) {
+//     document.location.replace("/patients");
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
 
 // const updatePetHandler = (event) => {
 //   event.preventDefault();
@@ -52,9 +51,9 @@ document
   .querySelector("#submit-dog-button")
   .addEventListener("click", addDogHandler);
 
-  document
-  .querySelector("#submit-cat-button")
-  .addEventListener("click", addCatHandler);
+// document
+//   .querySelector("#submit-cat-button")
+//   .addEventListener("click", addCatHandler);
 
 // document
 //   .querySelector("#update-button")
